d40db20fd8534caff95e704f8d1c6d4b21375e39
zk1013 <34581281+zk1013@users.noreply.github.com>
2020-04-30 04:27:15 +0800

    Delete index.php

--- deviceservices/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<title>Main menu</title>
-<body>
-	<a href="/deviceservices/tools/">Tools</a><br/>
-	<a href="/deviceservices/deviceActivation">iDevice activation</a><br/>
-	<a href="/deviceservices/checkUnbrickHealth">iDevice check unbrick health</a><br/>
-	<a href="/deviceservices/showSettings">iDevice show settings</a><br/>
-</body>
-</html>
